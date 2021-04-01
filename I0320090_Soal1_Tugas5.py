6a4afb7a911b71706a7c539968fda45bb66da98c
Salma Ardelia Irfani <salmaardelia24@gmail.com>
2021-04-01 04:50:57 -0700

    Tugas 5

---
+++ I0320090_Soal1_Tugas5.py
@@ -0,0 +1,9 @@
+# soal nomor 1 tugas 5
+
+#input nama dan jenis kelamin
+Nama = input("Masukkan nama pengguna : ")
+Jeniskelamin = input("Masukkan jenis kelamin pengguna : ")
+if (Jeniskelamin == "Perempuan"):
+    print("Selamat datang, Nyonya", Nama)
+else:
+    print("Selamat datang, Tuan", Nama)
---
+++ I0320090_Soal2_Tugas5.py
@@ -0,0 +1,16 @@
+# soal nomor 2 tugas 5
+
+Nama_pengguna = input("Masukkan nama pengguna : ")
+Nilai = int(input("Masukkan nilai berupa angka : "))
+if Nilai >= 85 and Nilai <= 100 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah A" )
+elif Nilai >= 80 and Nilai <85 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah A-" )
+elif Nilai >= 75 and Nilai <80 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah B+" )
+elif Nilai >= 70 and Nilai <75 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah B" )
+elif Nilai >= 65 and Nilai <70 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah C+")
+elif Nilai >= 60 and Nilai <65 :
+    print("Halo, ",Nama_pengguna,"! Nilai anda setelah dikonversi adalah C" )
